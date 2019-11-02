2db938accceaeb5ad6234727ec138f676cc78298
YounesJulien <younes.julien@courrier.uqam.ca>
2019-11-02 16:05:20 -0400

    Changements définitifs

--- Principal.java
+++ Principal.java
@@ -15,7 +15,7 @@ IMPORTANT : Il faut que le fichier se trouve dans le dossier "src" afin que le
     path fonctionne.
  */
 
-public class Principal {
+public class gitPrincipal {
 
     /*
     Lance le programme.
